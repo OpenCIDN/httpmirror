e5150d7fce84d665012b27cc5a40e79073c24865
Shiming Zhang <wzshiming@hotmail.com>
2025-11-14 10:21:53 +0800

    No longer strictly verify domain

--- mirror.go
+++ mirror.go
@@ -163,7 +163,7 @@ func (m *MirrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = urlpath
 	}
 
-	if !strings.Contains(host, ".") || !isValidDomain(host) {
+	if !strings.Contains(host, ".") {
 		m.notFoundResponse(w, r)
 		return
 	}
@@ -675,32 +675,4 @@ func (m *MirrorHandler) proxyDial(ctx context.Context, network, address string) 
 		proxyDial = dialer.DialContext
 	}
 	return proxyDial(ctx, network, address)
-}
-
-// isValidDomain validates if input string is a valid domain name.
-func isValidDomain(host string) bool {
-	// See RFC 1035, RFC 3696.
-	host = strings.TrimSpace(host)
-	if len(host) == 0 || len(host) > 255 {
-		return false
-	}
-	// host cannot start or end with "-"
-	if host[len(host)-1:] == "-" || host[:1] == "-" {
-		return false
-	}
-	// host cannot start or end with "_"
-	if host[len(host)-1:] == "_" || host[:1] == "_" {
-		return false
-	}
-	// host cannot start with a "."
-	if host[:1] == "." {
-		return false
-	}
-	// All non alphanumeric characters are invalid.
-	if strings.ContainsAny(host, "`~!@#$%^&*()+={}[]|\\\"';:><?/") {
-		return false
-	}
-	// No need to regexp match, since the list is non-exhaustive.
-	// We let it valid and fail later.
-	return true
 }
